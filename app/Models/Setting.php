cb5cf4c7a83e40f054e094d770dced9e6719be1b
XAPHNE <suvnkr.xaphene@gmail.com>
2026-03-04 02:28:46 +0530

    Brought under audit

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Setting extends Model
+class Setting extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
     protected $fillable = ['key', 'value'];
 
     /**
